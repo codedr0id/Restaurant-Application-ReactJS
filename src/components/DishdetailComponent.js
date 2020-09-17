c614bdcba66c5fb15b71ae56b702252643f68473
Mehul Lathi <mehullathi1999@gmail.com>
2020-09-17 22:32:48 +0530

    React Assignment-1

---
+++ src/components/DishdetailComponent.js
@@ -0,0 +1,80 @@
+import React, { Component } from 'react';
+import { Media } from 'reactstrap';
+import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+
+class DishDetail extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+        }
+    }
+
+    renderDish(dish) {
+        if (dish != null)
+            return (
+              <div className="col-12 col-md-5 m-1">
+                <Card>
+                    <CardImg top src={dish.image} alt={dish.name} />
+                    <CardBody>
+                        <CardTitle>{dish.name}</CardTitle>
+                        <CardText>{dish.description}</CardText>
+                    </CardBody>
+                </Card>
+              </div>
+            );
+        else
+            return (
+                <div></div>
+            );
+    }
+
+    renderComments(array) {
+        if (array.length != 0) {
+            return (
+                <div className="col-12 col-md m-2">
+                    <h4>Comments</h4>
+                    {array.map(comment => (
+                        <ul className="list-unstyled">
+                            <li>
+                                <p>{comment.comment}</p>
+                                <p>-- {comment.author} , {comment.date=new Intl.DateTimeFormat('en-US', {
+                                    year:'numeric',
+                                    month: 'short',
+                                    day: '2-digit'
+                                }).format(new Date(Date.parse(comment.date)))}</p>
+                            </li>
+                        </ul>
+                    )
+                  )}
+                </div>
+            );
+        }
+        else {
+            return (
+                <div></div>
+            );
+        }
+    }
+    render() {
+        let dish;
+        if (this.props.selectedDish) {
+            dish = (
+                <div className="row">
+                    {this.renderDish(this.props.selectedDish)}
+                    {this.renderComments(this.props.selectedDish.comments)}
+                </div>
+            )
+        } else {
+            dish = <div></div>
+        }
+        return (
+            <div className="container">
+                {dish}
+            </div>
+        );
+    }
+}
+
+export default DishDetail;
